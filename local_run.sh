c6e3093ab24faa0aee6f9250cc9ec6e740319668
tpmccallum <tim.mccallum@fermyon.com>
2025-03-04 12:51:40 +1000

    Update details

--- local_run.sh
+++ local_run.sh
@@ -4,7 +4,7 @@ echo "Installing dependencies with Poetry..."
 poetry install --no-root
 
 echo "Running the generator script..."
-poetry run python script.py
+poetry run python3 script.py
 
 echo "Serving the generated content at http://localhost:8000"
-python -m http.server --directory dist 8000
+python3 -m http.server --directory dist 8000
